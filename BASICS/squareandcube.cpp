879498872d4617605114b40489e85940da410a79
Sundara Vadivel <sundaravadivel015@gmail.com>
2026-03-18 23:25:02 +0530

    Basics:implemented basic conditonal and looping statements

---
+++ BASICS/squareandcube.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,s,c;
+    cout<<"Enter a number to get sqaure and cube:";
+    cin>>num;
+    s=num*num;
+    c=num*num*num;
+    cout<<"Square of the given number :"<<s<<"\n"<<"Cube of the given number :"<<c;
+}
