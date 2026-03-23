e11b1a3318c77965425c00bff4981f0935f8de17
Sundara Vadivel <sundaravadivel015@gmail.com>
2026-03-23 22:29:11 +0530

    to print prime numbers from 1to N

--- problems/primenumbers_1toN.cpp
+++ problems/primenumbers_1toN.cpp
@@ -5,8 +5,8 @@ int main(){
     cout << "Enter upper limit: ";
     cin >> limit;
     for(int i=2; i<=limit; i++){
-        flag = 0;
-        for(int j=2; j*j<=i; j++){
+        flag=0;
+        for(int j=2;j<i; j++){
             if(i%j==0){
                 flag = 1;
                 break;
