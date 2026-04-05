0ff298eb578c30144ba8e2c2bbda33b26b0fee31
Sundara Vadivel <sundaravadivel015@gmail.com>
2026-04-05 14:13:14 +0530

    Optimised:Second largest element in an array.

--- Week-3/second_largest_element_in_an_array.cpp
+++ Week-3/second_largest_element_in_an_array.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 int main()
 {
-    int arr[10]={23,45,6,7,54,6,4,99,7,6};
+    int arr[10]={2,3,4,5654,565,6764,4,45,45,56777};
     int larg=arr[0];
     int second_largest=-1;
     int len=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<len;i++){
+        if (arr[i]>larg )
+        {
+            second_largest=larg;
+            larg=arr[i];
+        }
+        else if(arr[i]<larg && arr[i]>second_largest){
+            second_largest=arr[i];
+        }
+        
+    }
+    
+    
+    /*for(int i=0;i<len;i++){
         if(arr[i]>larg){
             larg=arr[i];
         }
@@ -15,6 +28,9 @@ int main()
         if(arr[i]>second_largest && arr[i]!=larg){
             second_largest=arr[i];
         }
-    }
+    } */
+   
     cout<<"Second largest:"<<second_largest;
+
+
 } 
